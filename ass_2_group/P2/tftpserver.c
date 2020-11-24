a40a2a36e98d5a44c1e67a0bcafbeb9ac734b9fb
Vaibhav Garg <gargvaibhav64@gmail.com>
2020-11-24 11:50:02 +0530

    Add tftp message structure

--- ass_2_group/P2/tftpserver.c
+++ ass_2_group/P2/tftpserver.c
@@ -5,6 +5,50 @@
 #include <sys/socket.h>
 #include <netdb.h>
 
+enum opcode {
+     RRQ=1,
+     DATA,
+     ACK,
+     ERROR
+};
+
+/* tftp message structure */
+typedef union {
+
+    struct {
+        /* RRQ */
+        char filename_and_mode[514];
+    } read;     
+
+    struct {
+        /* DATA */
+        short block_number;
+        char data[512];
+    } data;
+
+    struct {
+        /* ACK */             
+        short block_number;
+    } ack;
+
+    struct {
+        /* ERROR */     
+        short error_code;
+        char error_string[512];
+    } error;
+
+} message;
+
+typedef struct {
+    short opcode;
+    message msg;
+} tftp_message;
+
+tftp_message t;
+
+/* base directory */
+char *base_directory;
+
 int main(int argc, char *argv[]) {
 
     if(argc < 2){
@@ -12,8 +56,36 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
-    int port = atoi(argv[1]);
+    int sockfd;
 
-    
+    struct sockaddr_in serv;
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("server: socket() error");
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+    port = htons(port);
+
+    serv.sin_family = AF_INET;
+    serv.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv.sin_port = port;
+
+    if (bind(sockfd, (struct sockaddr *) &serv, sizeof(serv)) == -1) {
+        perror("server: bind()");
+        close(sockfd);
+        exit(1);
+    }
+
+    printf("tftp server: listening on %d\n", ntohs(serv.sin_port));
+
+    while(1){
+
+
+
+
+    }
+
 
 }
