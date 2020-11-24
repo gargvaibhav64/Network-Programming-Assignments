3052bf783fb61882b23e9be64b709d5e604c8618
Vaibhav Garg <gargvaibhav64@gmail.com>
2020-11-25 01:17:16 +0530

    Handle client fd's

--- ass_2_group/P2/tftpserver.c
+++ ass_2_group/P2/tftpserver.c
@@ -67,8 +67,24 @@ clients client[MAX_CLIENTS];
 
 int cl_num;
 
-int tftp_recv_req(int s, tftp_message *m, struct sockaddr_in *sock, socklen_t *slen)
-{
+ssize_t tftp_send_data(int s, uint16_t block_number, uint8_t *data,
+                       ssize_t dlen, struct sockaddr_in *sock, socklen_t slen) {
+    tftp_message m;
+    ssize_t c;
+
+    m.opcode = htons(DATA);
+    m.msg.data.block_number = htons(block_number);
+    memcpy(m.msg.data.data, data, dlen);
+
+    if ((c = sendto(s, &m, 4 + dlen, 0,
+                    (struct sockaddr *) sock, slen)) < 0) {
+        perror("server: sendto()");
+    }
+
+    return c;
+}
+
+int tftp_recv(int s, tftp_message *m, struct sockaddr_in *sock, socklen_t *slen) {
     int c;
 
     if ((c = recvfrom(s, m, sizeof(*m), 0, (struct sockaddr *) sock, slen)) < 0
@@ -128,7 +144,7 @@ int main(int argc, char *argv[]) {
 
         if (FD_ISSET(sockfd, &rset)){
 
-            if ((msglen = tftp_recv_req(sockfd, &msg, &client_sock, &slen)) < 0) {
+            if ((msglen = tftp_recv(sockfd, &msg, &client_sock, &slen)) < 0) {
                 continue;
             }
 
@@ -142,14 +158,8 @@ int main(int argc, char *argv[]) {
             int opcode = ntohs(msg.opcode);
 
             if (opcode == RRQ) {
-                /* spawn a child process to handle the request */
-                // if (fork() == 0) {
-                tftp_handle_request(&msg, msglen, &client_sock, slen);
-                        // exit(0);
-                // }
 
-                client[cl_num].client_sock = client_sock;
-                client[cl_num].clilen = slen;
+                // tftp_handle_request(&msg, msglen, &client_sock, slen);
 
                 int clsockfd;
 
@@ -157,8 +167,6 @@ int main(int argc, char *argv[]) {
                     perror("server: socket()");
                     exit(1);
                 }
-
-                client[cl_num].sockfd = clsockfd;
 
                 char *filename, *end;
                 filename = msg.msg.read.filename_and_mode;
@@ -200,8 +208,18 @@ int main(int argc, char *argv[]) {
                     continue;
                 }
 
-                
+                FD_SET(clsockfd, &rset);
+                maxfdp1 = max(maxfdp1, clsockfd) + 1;
 
+                client[cl_num].block_num = 0;
+                client[cl_num].client_sock = client_sock;
+                client[cl_num].clilen = slen;
+                client[cl_num].sockfd = clsockfd;
+                client[cl_num].filefd = fd;
+                client[cl_num].prev_data_size = -1;
+                client[cl_num].port = 0;
+
+                cl_num++;
             }
 
             else {
@@ -209,6 +227,75 @@ int main(int argc, char *argv[]) {
                         inet_ntoa(client_sock.sin_addr), ntohs(client_sock.sin_port),
                         opcode);
                 // tftp_send_error(s, 0, "invalid opcode", &client_sock, slen);
+            }
+        } 
+        else {
+            for(int i = 0 ; i < cl_num ; i++){
+                if((client[i].filefd != -1 && FD_ISSET(client[i].sockfd, &rset)) 
+                        || client[i].prev_data_size == -1) {
+
+                    if(client[i].prev_data_size != -1){
+
+                        if ((msglen = tftp_recv(client[i].sockfd, &msg, &client[i].client_sock, &client[i].clilen)) < 0) {
+                            continue;
+                        }
+
+                        if (msglen < 4) { 
+                            printf("%s.%u: request with invalid size received\n",
+                                    inet_ntoa(client[i].client_sock.sin_addr), ntohs(client[i].client_sock.sin_port));
+                            // tftp_send_error(s, 0, "invalid request size", &client_sock, slen);
+                            continue;
+                        }
+
+                        if (ntohs(msg.opcode) == ERROR)  {
+                            printf("%s.%u: error message received: %u %s\n",
+                                    inet_ntoa(client[i].client_sock.sin_addr), ntohs(client[i].client_sock.sin_port),
+                                    ntohs(msg.msg.error.error_code), msg.msg.error.error_string);
+                            
+                            
+                            // Close connection
+                        }
+
+                        if (ntohs(m.opcode) != ACK)  {
+                            printf("%s.%u: invalid message during transfer received\n",
+                                inet_ntoa(client[i].client_sock.sin_addr), ntohs(client[i].client_sock.sin_port));
+                            // tftp_send_error(s, 0, "invalid message during transfer", client_sock, slen);
+                                
+                            // Close connection
+                        }
+
+                        if (ntohs(msg.msg.ack.block_number) != client[i].block_num) { // the ack number is too high
+                            printf("%s.%u: invalid ack number received\n", 
+                                inet_ntoa(client[i].client_sock.sin_addr), ntohs(client[i].client_sock.sin_port));
+                            // tftp_send_error(s, 0, "invalid ack number", client_sock, slen);
+
+                            // Close connection
+                        }
+
+                    }
+                    
+                    char data[512];
+
+                    int dlen;
+
+                    dlen = fread(data, 1, sizeof(data), client[i].filefd);
+
+                    if(dlen < 512){
+                        
+                    }
+                    
+                    int c;
+
+                    c = tftp_send_data(client[i].sockfd, client[i].block_num, &data, dlen, 
+                                            &client[i].client_sock, client[i].clilen);
+
+                    if (c < 0) {
+                        printf("%s.%u: transfer killed\n",
+                            inet_ntoa(client[i].client_sock.sin_addr), ntohs(client[i].client_sock.sin_port));
+
+                        // Close connection
+                    }
+                }
             }
         }
 
